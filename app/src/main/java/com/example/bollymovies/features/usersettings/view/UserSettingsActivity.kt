6557e1e6ef65c9edc7b3be56cc06788324f29da8
Victor Motta Del Bianco <victormottabianco@gmail.com>
2021-08-16 19:21:48 -0300

    tela de usuario implementada

---
+++ app/src/main/java/com/example/bollymovies/features/usersettings/view/UserSettingsActivity.kt
@@ -0,0 +1,16 @@
+package com.example.bollymovies.features.usersettings.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bollymovies.R
+import com.example.bollymovies.databinding.ActivityUserSettingsBinding
+
+class UserSettingsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityUserSettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUserSettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
--- app/src/main/java/com/example/bollymovies/features/watchedmovies/view/WatchedMoviesFragment.kt
+++ app/src/main/java/com/example/bollymovies/features/watchedmovies/view/WatchedMoviesFragment.kt
@@ -7,12 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.bollymovies.R
-import com.example.bollymovies.adapter.MyListAdapter
 import com.example.bollymovies.adapter.WatchedMoviesAdapter
-import com.example.bollymovies.databinding.FragmentHomeBinding
 import com.example.bollymovies.databinding.FragmentWatchedMoviesBinding
 import com.example.bollymovies.datamodels.Movie
 import com.example.bollymovies.features.watchedmovies.viewmodel.WatchedMoviesViewModel
