62b7577ad7257b71c1d348fae93cb907275105dd
Victor Motta Del Bianco <victormottabianco@gmail.com>
2021-10-31 21:36:40 -0300

    shimmer effect aplicado (com handler)

--- app/src/main/java/com/example/bollymovies/features/home/view/HomeFragment.kt
+++ app/src/main/java/com/example/bollymovies/features/home/view/HomeFragment.kt
@@ -2,6 +2,8 @@ package com.example.bollymovies.features.home.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -22,6 +24,9 @@ class HomeFragment : BaseFragment() {
 
     private var binding: FragmentHomeBinding? = null
     private lateinit var viewModel: HomeViewModel
+    private val takeShimmerTime = 1500L
+    private lateinit var handler: Handler
+
 
 
     private val nowPlayingAdapter: HomeAdapter by lazy {
@@ -64,6 +69,8 @@ class HomeFragment : BaseFragment() {
             viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
             viewModel.command = command
 
+            handler = Handler(Looper.getMainLooper())
+
             setupRecyclerViews()
             loadNowPlaying()
             loadPopular()
@@ -73,35 +80,63 @@ class HomeFragment : BaseFragment() {
     }
 
     private fun setupRecyclerViews() {
-        binding?.vgCardsListLancamentos?.apply {
+        binding?.vgCardsListNowPlaying?.apply {
             layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false)
             adapter = nowPlayingAdapter
+            binding?.shimmerNowPlaying?.startShimmer()
         }
         binding?.vgCardsListPopular?.apply {
             layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false)
             adapter = popularAdapter
+            binding?.shimmerPopular?.startShimmer()
         }
         binding?.vgCardsListTopRated?.apply {
             layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false)
             adapter = topRatedAdapter
+            binding?.shimmerTopRated?.startShimmer()
         }
     }
 
     private fun loadNowPlaying() {
         viewModel.nowPlayingPagedList?.observe(viewLifecycleOwner, { pagedList ->
-            nowPlayingAdapter.submitList(pagedList)
+//            binding?.shimmerNowPlaying?.stopShimmer()
+//            binding?.shimmerNowPlaying?.visibility = View.GONE
+//            binding?.vgCardsListNowPlaying?.visibility = View.VISIBLE
+            nowPlayingAdapter.currentList?.clear()
+            nowPlayingAdapter.submitList(pagedList, null)
+            nowPlayingAdapter.notifyDataSetChanged()
+            handler.postDelayed({ binding?.shimmerNowPlaying?.visibility = View.GONE },takeShimmerTime)
+
+
         })
     }
 
     private fun loadPopular() {
         viewModel.popularPagedList?.observe(viewLifecycleOwner, { pagedList ->
+//            binding?.shimmerPopular?.stopShimmer()
+//            binding?.shimmerPopular?.visibility = View.GONE
+//            binding?.vgCardsListPopular?.visibility = View.VISIBLE
+            popularAdapter.currentList?.clear()
             popularAdapter.submitList(pagedList)
+            popularAdapter.notifyDataSetChanged()
+            handler.postDelayed({ binding?.shimmerPopular?.visibility = View.GONE },takeShimmerTime)
+
+
+
         })
     }
 
     private fun loadTopRated() {
         viewModel.topRatedPagedList?.observe(viewLifecycleOwner, { pagedList ->
+//            binding?.shimmerTopRated?.stopShimmer()
+//            binding?.shimmerTopRated?.visibility = View.GONE
+//            binding?.vgCardsListTopRated?.visibility = View.VISIBLE
+            topRatedAdapter.currentList?.clear()
             topRatedAdapter.submitList(pagedList)
+            topRatedAdapter.notifyDataSetChanged()
+            handler.postDelayed({ binding?.shimmerTopRated?.visibility = View.GONE },takeShimmerTime)
+
+
         })
     }
 
