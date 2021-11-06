724a20f8033719f2b03d3728b3ef470575cb971c
Victor Motta Del Bianco <victormottabianco@gmail.com>
2021-11-05 21:34:41 -0300

    myList and videos updated

--- app/src/main/java/com/example/bollymovies/features/moviedetails/view/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/bollymovies/features/moviedetails/view/MovieDetailsActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.content.pm.PackageManager.*
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.MutableLiveData
@@ -22,12 +21,10 @@ import com.example.bollymovies.features.moviedetails.viewmodel.MovieDetailsViewM
 import com.example.bollymovies.model.Movie
 import com.example.bollymovies.utils.Command
 import com.example.bollymovies.utils.ConstantsApp.Home.KEY_INTENT_MOVIE_ID
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.utils.loadOrCueVideo
-import okhttp3.internal.notify
-import okhttp3.internal.wait
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 
 class MovieDetailsActivity : AppCompatActivity() {
@@ -39,6 +36,8 @@ class MovieDetailsActivity : AppCompatActivity() {
     private var sharedTitle: String? = null
     private var overview: String? = null
     private var year: String? = null
+    private lateinit var youtubePlayer: YouTubePlayer
+    private lateinit var youtubePlayerView: YouTubePlayerView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,6 +52,7 @@ class MovieDetailsActivity : AppCompatActivity() {
             viewModel.getMyListMoviesDb()
             viewModel.getWatchedMoviesDb()
         }
+
         setupObservables()
         binding.ivArrowBack.setOnClickListener {
             finish()
@@ -71,6 +71,14 @@ class MovieDetailsActivity : AppCompatActivity() {
             year = binding.tvYear.text.toString()
             goToShare()
         }
+        youtubePlayerView = binding.youtubePlayerDetail
+        youtubePlayerView.initialize(object :
+            AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+               youtubePlayer = youTubePlayer
+                binding.btTrailerFilmsSeries.isEnabled = true
+            }
+        })
     }
 
     private fun setupObservables() {
@@ -103,6 +111,7 @@ class MovieDetailsActivity : AppCompatActivity() {
 
             if (it.videos!!.results.isNotEmpty()){
                 binding.btTrailerFilmsSeries.isVisible = true
+                binding.btTrailerFilmsSeries.isEnabled = false
                 }
 
             binding.cbMyListMovies.setOnClickListener {
@@ -197,7 +206,11 @@ class MovieDetailsActivity : AppCompatActivity() {
                     .into(ivMovieDetailsImage)
 
                 tvMovieName.text = movie.title
-                tvDescriptionText.text = movie.overview
+                if (!movie.overview.isNullOrEmpty()) {
+                    tvDescriptionText.text = movie.overview
+                } else {
+                    tvDescriptionText.text = "Sinopse não encontrada"
+                }
                 tvYear.text = movie.release_date.toString().getFirst4Chars()
                 tvTime.text = textIsNotNull(movie.runtime.toString())
                 movie.vote_average?.let {
@@ -212,29 +225,24 @@ class MovieDetailsActivity : AppCompatActivity() {
     }
 
     private fun setupVideo(movie: Movie) {
-            val youtubePlayerView = binding.youtubePlayerDetail
             lifecycle.addObserver(youtubePlayerView)
-            val youtube = movie.videos!!.results.last()
+            val trailer = movie.videos!!.results.last()
             binding.apply {
                 btTrailerFilmsSeries.isVisible = false
                 clYoutube.isVisible = true
-                youtubePlayerView.initialize(object :
-                    AbstractYouTubePlayerListener() {
-                    override fun onReady(youTubePlayer: YouTubePlayer) {
-                        youtube.key.let { key -> youTubePlayer.loadOrCueVideo(lifecycle, key, 0f) }
+                        trailer.key.let { key -> youtubePlayer.loadOrCueVideo(lifecycle, key, 0f) }
                         btnClose.setOnClickListener{
                             clYoutube.isVisible = false
-                            youTubePlayer.pause()
+                            btTrailerFilmsSeries.isVisible = true
+                            youtubePlayer.pause()
                         }
-                    }
-                })
                 youtubePlayerView.isFullScreen()
             }
     }
 
 
     private fun textIsNotNull(text: String): String {
-        if (text != "null") {
+        if (text != "null" && text != "0") {
             return "$text min."
         } else {
             return " "
